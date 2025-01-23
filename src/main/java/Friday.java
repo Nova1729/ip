71a879d0a655ed09b5f881c5a6b98871c7cdd437
Just to Use R and IDLE <novaji1729@gmail.com>
2025-01-23 17:07:04 +0800

    Code can now store whatever text entered by the user and display them back to the user when requested

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Friday {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Friday");
         System.out.println(" What can I do for you?");
@@ -10,19 +13,31 @@ public class Friday {
 
         while (true) {
             String userInput = scanner.nextLine();
+
             if (userInput.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            } else {
+
+            } else if (userInput.equals("list")) {
                 System.out.println("____________________________________________________________");
-                System.out.println(" " + userInput);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+
+            } else {
+                tasks[taskCount] = userInput;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + userInput);
                 System.out.println("____________________________________________________________");
             }
         }
         scanner.close();
     }
 }
+
 
 
