f463142cae9d410c5d337b3eaad402ff5c1ae3f5
Just to Use R and IDLE <novaji1729@gmail.com>
2025-01-23 17:34:51 +0800

    Add support for tracking three types of tasks (Todo, Deadline and Event)

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Friday {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Task[] tasks = new Task[100];
-        int numTasks = 0;
+        Task[] tasks = new Task[100]; // Array to store tasks
+        int numTasks = 0; // Counter for the number of tasks
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Friday");
@@ -53,18 +53,48 @@ public class Friday {
                     System.out.println(" Invalid task number.");
                 }
 
-            } else {
-                tasks[numTasks] = new Task(userInput); // Add a new task
+            // code for this
+            } else if (inputParts[0].equals("todo")) {
+                tasks[numTasks] = new Todo(inputParts[1]); // Create a new ToDo task
                 numTasks++;
                 System.out.println("____________________________________________________________");
-                System.out.println(" added: " + userInput);
+                System.out.println(" Got it. I've added this task:");
+                System.out.println("   " + tasks[numTasks - 1]);
+                System.out.println(" Now you have " + numTasks + " tasks in the list.");
+                System.out.println("____________________________________________________________");
+
+            // code for deadline
+            } else if (inputParts[0].equals("deadline")) {
+                String[] details = inputParts[1].split(" /by ", 2);
+                tasks[numTasks] = new Deadline(details[0], details[1]);
+                numTasks++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" Got it. I've added this task:");
+                System.out.println("   " + tasks[numTasks - 1]);
+                System.out.println(" Now you have " + numTasks + " tasks in the list.");
+                System.out.println("____________________________________________________________");
+
+            // Code for event
+            } else if (inputParts[0].equals("event")) {
+                String[] details = inputParts[1].split(" /from | /to ", 3);
+                tasks[numTasks] = new Event(details[0], details[1], details[2]);
+                numTasks++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" Got it. I've added this task:");
+                System.out.println("   " + tasks[numTasks - 1]);
+                System.out.println(" Now you have " + numTasks + " tasks in the list.");
+                System.out.println("____________________________________________________________");
+
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" I'm sorry, I don't recognize that command.");
                 System.out.println("____________________________________________________________");
             }
         }
-
         scanner.close();
     }
 }
+
 
 
 
