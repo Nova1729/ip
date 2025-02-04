303088ffce74a164b460884ec8dfd153d33b75ed
Just to Use R and IDLE <novaji1729@gmail.com>
2025-02-04 15:48:21 +0800

    Add java Doc for UnmarkCommand

--- src/main/java/chatbot/commands/UnmarkCommand.java
+++ src/main/java/chatbot/commands/UnmarkCommand.java
@@ -4,13 +4,30 @@ import chatbot.tasks.*;
 import chatbot.Ui;
 import chatbot.Storage;
 
+/**
+ * Represents a command to unmark a specific task as not done.
+ */
 public class UnmarkCommand extends Command {
     private final int index;
 
+    /**
+     * Constructs an {@code UnmarkCommand} with the specified task index.
+     *
+     * @param index The index of the task to be unmarked as not done (1-based index).
+     */
     public UnmarkCommand(int index) {
         this.index = index;
     }
 
+    /**
+     * Executes the unmark command by marking the specified task as not done.
+     *
+     * @param tasks   The {@link TaskList} containing the user's tasks.
+     * @param ui      The {@link Ui} responsible for user interaction.
+     * @param storage The {@link Storage} that manages saving and loading of tasks.
+     * @throws IndexOutOfBoundsException If the task index is out of valid range.
+     * @throws Exception If an unexpected error occurs while unmarking the task.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws Exception {
         if (index < 1 || index > tasks.size()) {
@@ -23,5 +40,6 @@ public class UnmarkCommand extends Command {
         storage.save(tasks.getTasks());
     }
 }
+
 
 
