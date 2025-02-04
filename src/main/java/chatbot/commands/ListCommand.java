595c0a24d6c599b95a281d4d436c2ed553d0b010
Just to Use R and IDLE <novaji1729@gmail.com>
2025-02-04 15:44:32 +0800

    Add Java Doc for ListCommand

--- src/main/java/chatbot/commands/ListCommand.java
+++ src/main/java/chatbot/commands/ListCommand.java
@@ -4,7 +4,18 @@ import chatbot.tasks.*;
 import chatbot.Ui;
 import chatbot.Storage;
 
+/**
+ * Represents a command to list all tasks currently stored in the task list.
+ */
 public class ListCommand extends Command {
+
+    /**
+     * Executes the list command by displaying all tasks in the task list.
+     *
+     * @param tasks   The {@link TaskList} containing the user's tasks.
+     * @param ui      The {@link Ui} responsible for user interaction.
+     * @param storage The {@link Storage} that manages saving and loading of tasks (not used in this command).
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         ui.showMessage("Here are the tasks in your list:");
@@ -13,4 +24,5 @@ public class ListCommand extends Command {
         }
     }
 }
+
 
