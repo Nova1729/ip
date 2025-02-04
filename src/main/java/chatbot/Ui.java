701e1cc7a23de7faece4f4d53bb3e1c2494a6cb3
Just to Use R and IDLE <novaji1729@gmail.com>
2025-02-04 10:40:20 +0800

    Add Java doc for Ui

--- src/main/java/chatbot/Ui.java
+++ src/main/java/chatbot/Ui.java
@@ -2,13 +2,23 @@ package chatbot;
 
 import java.util.Scanner;
 
+/**
+ * The Ui class handles interactions with the user.
+ * It provides methods to display messages, read user inputs, and show error messages.
+ */
 public class Ui {
     private final Scanner scanner;
 
+    /**
+     * Constructs a Ui object and initializes the scanner for user input.
+     */
     public Ui() {
         this.scanner = new Scanner(System.in);
     }
 
+    /**
+     * Displays the welcome message to the user.
+     */
     public void showWelcome() {
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Friday");
@@ -16,18 +26,36 @@ public class Ui {
         System.out.println("____________________________________________________________");
     }
 
+    /**
+     * Reads a command entered by the user.
+     *
+     * @return The command string entered by the user.
+     */
     public String readCommand() {
         return scanner.nextLine();
     }
 
+    /**
+     * Displays a line divider for formatting output.
+     */
     public void showLine() {
         System.out.println("____________________________________________________________");
     }
 
+    /**
+     * Displays a message to the user.
+     *
+     * @param message The message to be displayed.
+     */
     public void showMessage(String message) {
         System.out.println(message);
     }
 
+    /**
+     * Displays an error message to the user.
+     *
+     * @param error The error message to be displayed.
+     */
     public void showError(String error) {
         System.out.println("OOPS!!! " + error);
     }
