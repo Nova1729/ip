7813c774521b4de1ccdc9b0c2f480bafe4f8b43a
Just to Use R and IDLE <novaji1729@gmail.com>
2025-02-04 15:41:24 +0800

    Add Java Doc for DeleteCommand

--- src/main/java/chatbot/commands/DeleteCommand.java
+++ src/main/java/chatbot/commands/DeleteCommand.java
@@ -4,13 +4,32 @@ import chatbot.tasks.*;
 import chatbot.Ui;
 import chatbot.Storage;
 
+/**
+ * Represents a command to delete a task from the task list.
+ */
 public class DeleteCommand extends Command {
+    /** The index of the task to be deleted. */
     private final int index;
 
+    /**
+     * Constructs a {@code DeleteCommand} with the specified task index.
+     *
+     * @param index The index of the task to be deleted (1-based index).
+     */
     public DeleteCommand(int index) {
         this.index = index;
     }
 
+    /**
+     * Executes the delete command by removing the task from the task list,
+     * displaying a confirmation message, and updating the storage.
+     *
+     * @param tasks   The {@link TaskList} containing the tasks.
+     * @param ui      The {@link Ui} responsible for user interaction.
+     * @param storage The {@link Storage} that manages saving and loading of tasks.
+     * @throws IndexOutOfBoundsException If the index is out of range.
+     * @throws Exception If an error occurs while saving the updated task list.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws Exception {
         if (index < 1 || index > tasks.size()) {
@@ -23,4 +42,5 @@ public class DeleteCommand extends Command {
         storage.save(tasks.getTasks());
     }
 }
+
 
