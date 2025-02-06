862caf6625d9c834cd0b08cc719c65aa0d59fafa
Just to Use R and IDLE <novaji1729@gmail.com>
2025-02-06 17:03:07 +0800

    Update ExitCommand to produce output on GUI

--- src/main/java/chatbot/commands/ExitCommand.java
+++ src/main/java/chatbot/commands/ExitCommand.java
@@ -9,29 +9,13 @@ import chatbot.tasks.TaskList;
  */
 public class ExitCommand extends Command {
 
-    /**
-     * Executes the exit command by displaying a farewell message.
-     *
-     * @param tasks   The {@link TaskList} containing the tasks (not used in this command).
-     * @param ui      The {@link Ui} responsible for user interaction.
-     * @param storage The {@link Storage} that manages saving and loading of tasks (not used in this command).
-     * @return
-     */
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) {
-        ui.showMessage("Bye. Hope to see you again soon!");
-        return null;
+        return "Bye. Hope to see you again soon!";
     }
 
-    /**
-     * Indicates that this command will terminate the chatbot program.
-     *
-     * @return {@code true}, indicating that the program should exit.
-     */
     @Override
     public boolean isExit() {
         return true;
     }
 }
-
-
